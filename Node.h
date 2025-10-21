fab853b992fc8916d7a8a4007ed4382a77f0b0d3
Andreu <amarcab1@upv.edu.es>
2025-10-21 18:29:58 +0200

    Clase Node implementada

---
+++ Node.h
@@ -0,0 +1,19 @@
+#include <ostream>
+#include "List.h"
+
+template <typename T>
+class Node
+{
+    public:
+        T data;
+        Node<T>* next;
+
+        Node(T data, Node<T> *next = nullptr) : data(data), next(next) {}
+
+            friend std::ostream &
+            operator<<(std::ostream &out, const Node<T> &node)
+            {
+                out << node.data;
+                return out;
+            }
+};
